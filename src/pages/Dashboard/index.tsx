66d2bec4667229fbe4c67e25a2d976fec14e999a
Gabriel Freitas <gabriel.freitas@bemobi.com.br>
2020-05-26 21:01:26 -0300

    add repository name params to url

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, FormEvent} from 'react';
 import {FiChevronRight} from 'react-icons/fi';
+import {Link} from 'react-router-dom'
 import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg';
@@ -76,7 +77,7 @@ const Dashboard: React.FC = () => {
 
         <Repositories>
             {repositories.map( repository => (
-                <a key={repository.full_name} href="">
+                <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
                 <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
                 <div>
                     <strong>{repository.full_name}</strong>
@@ -84,7 +85,7 @@ const Dashboard: React.FC = () => {
                 </div>
 
                 <FiChevronRight size={20} />
-            </a>
+            </Link>
             ))}
 
         </Repositories>
